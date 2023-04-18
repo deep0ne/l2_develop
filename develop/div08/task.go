f72a37a3f96b9e2034cde2221151062d8dfcad77
Alexander <kozhukhovmib@gmail.com>
2023-04-18 18:21:58 +0300

    TODO: pipelines, exec/fork

---
+++ develop/div08/task.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		scanner.Scan()
+		text := strings.Split(scanner.Text(), " ")
+		command := text[0]
+		switch command {
+		case "cd":
+			err := os.Chdir(text[1])
+			if err != nil {
+				fmt.Println("Cannot change directory. Change your input.")
+				fmt.Println("Type \"pwd\" if you want to know your current directory")
+			}
+
+		case "pwd":
+			dir, err := os.Getwd()
+			if err != nil {
+				fmt.Println("Could not get current directory...")
+			}
+			fmt.Println(dir)
+
+		case "echo":
+			fmt.Println(strings.Join(text[1:], " "))
+
+		case "ps":
+			processes, err := exec.Command("ps", text[1:]...).Output()
+			if err != nil {
+				fmt.Println("There are no processes")
+			}
+			fmt.Println(string(processes))
+		case "kill":
+			var pid int
+			for _, arg := range text[1:] {
+				if arg[0] != '-' {
+					pidConverted, err := strconv.Atoi(arg)
+					if err != nil {
+						fmt.Println("Wrong PID. To find PID you can type \"ps -e\"")
+					}
+					pid = pidConverted
+				}
+			}
+
+			process, err := os.FindProcess(pid)
+			if err != nil {
+				fmt.Println("Wrong PID. To find PID you can type \"ps -e\"")
+			}
+			err = process.Kill()
+			if err != nil {
+				fmt.Println("Could not kill process. Are you sure your PID is correct?")
+			} else {
+				fmt.Printf("Process %d is killed!\n", pid)
+			}
+		case "quit":
+			return
+		}
+	}
+}
