63db03b9a9240deb61e121516af3f53692e2462d
Alexander <kozhukhovmib@gmail.com>
2023-04-19 19:20:27 +0300

    TODO: tests and fixes

---
+++ develop/dev05/grep/grep.go
@@ -0,0 +1,91 @@
+package grep
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type SearchOptions struct {
+	AfterFlag, BeforeFlag, ContextFlag, CountFlag  int
+	IgnoreFlag, InvertFlag, FixedFlag, LineNumFlag bool
+}
+
+func FormRegex(pattern string, options SearchOptions) string {
+	var regex string
+
+	if options.IgnoreFlag {
+		regex = regexp.QuoteMeta(strings.ToLower(pattern))
+	} else {
+		regex = regexp.QuoteMeta(pattern)
+	}
+
+	if options.FixedFlag {
+		regex = "\\b" + regex + "\\b"
+	} else {
+		regex = "\\w*" + regex + "\\w*"
+	}
+
+	if options.BeforeFlag > 0 {
+		regex = "(?:\\w+\\W+){" + strconv.Itoa(options.BeforeFlag) + "}" + regex
+	}
+
+	if options.AfterFlag > 0 {
+		regex = regex + "(?:\\W+\\w+){" + strconv.Itoa(options.AfterFlag) + "}"
+	}
+
+	if options.ContextFlag > 0 {
+		regex = "(?:\\w+\\W+){" + strconv.Itoa(options.ContextFlag) + "}" + regex + "(?:\\W+\\w+){" + strconv.Itoa(options.ContextFlag) + "}"
+	}
+
+	return regex
+}
+
+func Grep(regex *regexp.Regexp, file *os.File, options SearchOptions) error {
+	var iterate int
+	scanner := bufio.NewScanner(file)
+	findings := make([]string, 0, options.CountFlag)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		if options.IgnoreFlag {
+			text = strings.ToLower(text)
+		}
+		finds := regex.FindAllString(text, -1)
+
+		if options.InvertFlag {
+			notFound := make(map[string]struct{})
+			for _, f := range finds {
+				for _, word := range strings.Split(f, " ") {
+					notFound[word] = struct{}{}
+				}
+			}
+			notFoundString := ""
+			for _, word := range strings.Split(text, " ") {
+				if _, ok := notFound[word]; !ok {
+					notFoundString += word + " "
+				}
+			}
+			findings = append(findings, notFoundString)
+		} else {
+			findings = append(findings, strings.Join(finds, " "))
+		}
+	}
+
+	if options.CountFlag > 0 {
+		iterate = options.CountFlag
+	} else {
+		iterate = len(findings)
+	}
+
+	for line := 0; line < iterate; line++ {
+		if options.LineNumFlag {
+			fmt.Print(line+1, ": ")
+		}
+		fmt.Println(findings[line])
+	}
+	return nil
+}
---
+++ develop/dev05/task.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"dev05/grep"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+)
+
+func main() {
+
+	var (
+		AfterFlag, BeforeFlag, ContextFlag, CountFlag  int
+		IgnoreFlag, InvertFlag, FixedFlag, LineNumFlag bool
+	)
+
+	flag.IntVar(&AfterFlag, "A", 0, "Usage: \"after\" - печатать N строк после совпадения")
+	flag.IntVar(&BeforeFlag, "B", 0, "Usage: \"before\" - печатать N строк до совпадения")
+	flag.IntVar(&ContextFlag, "C", 0, "Usage: - печатать N строк до и после совпадения")
+	flag.IntVar(&CountFlag, "c", 0, "Usage: \"after\" - печатать N строк после совпадения")
+	flag.BoolVar(&IgnoreFlag, "i", false, "Usage: \"ignore-case\" - игнорировать регистр")
+	flag.BoolVar(&InvertFlag, "v", false, "Usage: \"invert\" - Исключает вместо совпадения")
+	flag.BoolVar(&FixedFlag, "F", false, "Usage: \"fixed\" - искать только полное совпадение, а не паттерн")
+	flag.BoolVar(&LineNumFlag, "n", false, "Usage: \"line num\" - печатать номер строки")
+
+	flag.Parse()
+
+	if ContextFlag != 0 && (AfterFlag != 0 || BeforeFlag != 0) {
+		fmt.Fprintf(os.Stderr, "Флаг \"C\" может быть использован, только если флаги \"A\" и \"B\" не были введены.")
+		os.Exit(1)
+	}
+
+	search := os.Args[len(os.Args)-1]
+	searchOptions := grep.SearchOptions{
+		AfterFlag:   AfterFlag,
+		BeforeFlag:  BeforeFlag,
+		ContextFlag: ContextFlag,
+		CountFlag:   CountFlag,
+		IgnoreFlag:  IgnoreFlag,
+		InvertFlag:  InvertFlag,
+		FixedFlag:   FixedFlag,
+		LineNumFlag: LineNumFlag,
+	}
+
+	regex := grep.FormRegex(search, searchOptions)
+	re := regexp.MustCompile(regex)
+
+	file, err := os.Open("words.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = grep.Grep(re, file, searchOptions)
+
+}
