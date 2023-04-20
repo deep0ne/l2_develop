5c41319a7aaf114c465fbcbc1ea9321c9506b865
Alexander <kozhukhovmib@gmail.com>
2023-04-20 15:06:34 +0300

    done

--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -3,18 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/beevik/ntp"
 )
 
 const NTPServer = "0.beevik-ntp.pool.ntp.org"
 
-func main() {
+func GetNTPTime() time.Time {
 	t, err := ntp.Time(NTPServer)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Ошибка получения времени", err)
 		os.Exit(1)
 	}
+	return t
+}
 
-	fmt.Println(t.Format("2006-01-02 15:04:05.000"))
+func main() {
+	fmt.Println(GetNTPTime().Format("2006-01-02 15:04:05.000"))
 }
