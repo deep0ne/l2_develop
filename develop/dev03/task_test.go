29c207a2b3d4797246b421045579f62abcac7652
Alexander <kozhukhovmib@gmail.com>
2023-04-24 00:48:02 +0300

    TODO: extras

---
+++ develop/dev03/task_test.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+func TestGrep(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected []string
+		params   SortParams
+	}{
+		{
+			input: `abc def ghi
+ghi def abc
+def abc ghi
+def ghi abc`,
+			expected: []string{"abc def ghi", "def abc ghi", "def ghi abc", "ghi def abc"},
+			params:   SortParams{},
+		},
+		{
+			input: `1 2 3
+4 5 6
+7 8 9
+10 11 12
+13 14 15`,
+			expected: []string{"1 2 3", "10 11 12", "13 14 15", "4 5 6", "7 8 9"},
+			params:   SortParams{},
+		},
+		{
+			input: `1 2 3
+4 5 6
+7 8 9
+10 11 12
+13 14 15`,
+			expected: []string{"1 2 3", "4 5 6", "7 8 9", "10 11 12", "13 14 15"},
+			params: SortParams{
+				numerical: true,
+			},
+		},
+		{
+			input: `1 2 3
+4 5 6
+7 8 9
+10 11 12
+13 14 15`,
+			expected: []string{"13 14 15", "10 11 12", "7 8 9", "4 5 6", "1 2 3"},
+			params: SortParams{
+				numerical: true,
+				reverse:   true,
+			},
+		},
+		{
+			input: `1 5 3
+4 0 6
+1 2 4
+10 1 5
+666 3 0`,
+			expected: []string{"4 0 6", "10 1 5", "1 2 4", "666 3 0", "1 5 3"},
+			params: SortParams{
+				numerical: true,
+				column:    2,
+			},
+		},
+		{
+			input: `1 1 1
+1 2 3
+1 1 1
+3 2 1
+4 0 5
+6 4 2
+2 4 6
+`,
+			expected: []string{"2 4 6", "4 0 5", "1 2 3", "6 4 2", "3 2 1", "1 1 1"},
+			params: SortParams{
+				numerical: true,
+				column:    3,
+				reverse:   true,
+				unique:    true,
+				sorted:    true,
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		input, _ := os.Create("input.txt")
+		input.WriteString(tc.input)
+
+		fileInput, _ := os.Open("input.txt")
+		Sort(fileInput, tc.params)
+
+		fileOutput, _ := os.Open("sorted_input.txt")
+		scanner := bufio.NewScanner(fileOutput)
+		for _, expectedString := range tc.expected {
+			scanner.Scan()
+			text := scanner.Text()
+			if expectedString != text {
+				t.Errorf("Sort worked wrong. Expected %s. Got %s.", expectedString, text)
+			}
+		}
+	}
+
+}
